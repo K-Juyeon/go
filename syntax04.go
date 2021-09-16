4441b82f28504147ba67b2efb11d88e297987ecd
K-Juyeon <kimjuyeon0912@gmail.com>
2021-09-16 11:09:17 +0900

    v0.4

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time" //seed 생성용 패키지
 )
 
-//난수 추출된 수의 소수판정 프로그램 v0.3
+//난수 추출된 수의 소수판정 프로그램 v0.4
 //소수는 1과 자기 자신외에는 나누어 떨어지지 않는 수(0과 1 제외)
 func main() {
 	seed := time.Now().Unix()
@@ -19,10 +19,10 @@ func main() {
 	for i := 2; i < number; i++ { //1과 number일때 loop돌지 않음
 		if number%i == 0 {
 			isPrime = false
-			//count = count + 1
 		}
+		fmt.Print(i, " ")
 	}
-	if isPrime == true {
+	if isPrime { //비교 연산자 제거
 		fmt.Println(number, "는(은) 소수입니다.")
 	} else {
 		fmt.Println(number, "는(은) 소수가 아닙니다.")
